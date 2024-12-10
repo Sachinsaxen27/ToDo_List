09bc9edac034e7800d30dc28070851df59dc07c7
Sachin Saxena <saxenasachin186@gmail.com>
2024-12-10 20:06:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const taskarry = [];
+function addtask() {
+    const task = document.getElementById('yourtask')
+    if (task.value.trim() !== '') {
+        taskarry.push(task.value)
+        localStorage.setItem('taskarray',JSON.stringify(taskarry))
+        task.value = ''
+    } else {
+        console.log('Task cannot be empty')
+    }
+}
+function displayoutput() {
+    console.log(localStorage.getItem('taskarray'))
+    const output=JSON.parse(localStorage.getItem("taskarray"))||[]
+    const parent=document.getElementById('yourtodooutput')
+    for(let i=0;i<=output.length-1;i++){
+        const newdiv=document.createElement("div")
+        newdiv.textContent=taskarry[i]
+        parent.appendChild(newdiv)
+    }
+
+}
+{/* <input class="taskinput" id="yourpendingtask" readonly></input> */ }
